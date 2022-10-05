9219d31fdac7963d3a64fdf99e2846cb9d161ddc
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-10-05 09:35:47 +0300

    Completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const displayMovies = movies.map((movieProp) => (
+      <div key={movieProp.title}>
+        <h2>Name:{movieProp.title}</h2>
+        <p>Time:{movieProp.time}</p>
+        <ul>
+          {movieProp.genres.map((genre) => (
+         <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+      </div>
+  ));
+  
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {displayMovies}
+    </div>
+  );
 }
 
 export default Movies;
